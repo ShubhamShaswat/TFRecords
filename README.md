c97d95063d4d2f1899ae439b507b6fe03dd2439d
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-10-27 22:42:42 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TFRecords
 A library to create and view Tf records
 Create TFrecords dataset
+
+Working on creating more searlise examples for Image, Text..
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	nane='tfrecordpy',
+	versions='0.0.1'.
+	description='read and write tfrecord files'.
+	py_moduels=[''],
+	package_dir={'' : 'src'},
+)
